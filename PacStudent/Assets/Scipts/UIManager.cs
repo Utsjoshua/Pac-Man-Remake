ff78d6326a17ff471f2c48b70498c0ca77022fd5
Utsjoshua <Joshua.E.Isaraela@student.uts.edu.au>
2022-10-26 16:16:27 +1100

    fixed up duplication error

--- PacStudent/Assets/Scipts/UIManager.cs
+++ PacStudent/Assets/Scipts/UIManager.cs
@@ -7,10 +7,17 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public AudioSource Title;
+    private static GameObject obj;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (obj == null){
+            obj = gameObject;
+        }
+        else{
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -30,12 +37,15 @@ public class UIManager : MonoBehaviour
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode){
         if (scene.buildIndex == 0){
             Title.Play();
+            GameObject Level1Button = GameObject.FindGameObjectWithTag("Level1Button");
+            Button btn = Level1Button.GetComponent<Button>();
+		    btn.onClick.AddListener(LoadFirstlevel);
         }
         if (scene.buildIndex == 1){
             Title.Pause();
             Title.Stop();
-            GameObject button = GameObject.FindGameObjectWithTag("ExitButton");
-            Button btn = button.GetComponent<Button>();
+            GameObject Exitbutton = GameObject.FindGameObjectWithTag("ExitButton");
+            Button btn = Exitbutton.GetComponent<Button>();
 		    btn.onClick.AddListener(Exit);
             
         }
